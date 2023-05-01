df8711c9b03f00e0d66b481cce9916d9e724707c
wwwwwwwwt <1162933894@qq.com>
2023-05-01 19:19:10 +0800

    finish load balance

--- main/main.go
+++ main/main.go
@@ -2,7 +2,7 @@
  * @Author: zzzzztw
  * @Date: 2023-04-28 12:25:51
  * @LastEditors: Do not edit
- * @LastEditTime: 2023-05-01 15:42:47
+ * @LastEditTime: 2023-05-01 18:45:54
  * @FilePath: /TidyRpcByGo/main/main.go
  */
 package main
@@ -11,13 +11,13 @@ import (
 	"context"
 	"log"
 	"net"
-	"net/http"
 	"sync"
 	"time"
 	"tinyrpc"
+	"tinyrpc/xclient"
 )
 
-type Foo int
+/*type Foo int
 
 type Args struct{ Num1, Num2 int }
 
@@ -25,9 +25,9 @@ func (f Foo) Sum(args Args, reply *int) error {
 	//time.Sleep(2 * time.Second)
 	*reply = args.Num1 + args.Num2
 	return nil
-}
+}*/
 
-/*func startServer(addr chan string) {
+/*func startServer(addr chan string) { // test for timeout
 	var foo Foo
 	if err := tinyrpc.Register(&foo); err != nil {
 		log.Fatal("register error:", err)
@@ -68,7 +68,7 @@ func main() {
 	wg.Wait()
 }*/
 
-func startServer(addrCh chan string) {
+/*func startServer(addrCh chan string) { // test for httpdial and debug
 	var foo Foo
 	l, _ := net.Listen("tcp", ":9999")
 	_ = tinyrpc.Register(&foo)
@@ -103,4 +103,100 @@ func main() {
 	ch := make(chan string)
 	go call(ch)
 	startServer(ch)
+}*/
+
+//-------------------------------------------------------------------------------------
+// test for balance
+
+type Foo int
+
+type Args struct{ Num1, Num2 int }
+
+func (f Foo) Sum(args Args, reply *int) error {
+	*reply = args.Num1 + args.Num2
+	return nil
+}
+
+//验证Xclient的超时是否能正常工作
+func (f Foo) Sleep(args Args, reply *int) error {
+	time.Sleep(time.Second * time.Duration(args.Num1))
+	*reply = args.Num1 + args.Num2
+	return nil
+}
+
+func startServer(addrCh chan string) {
+	var foo Foo
+	l, _ := net.Listen("tcp", ":0")
+	server := tinyrpc.NewServer()
+	_ = server.Register(&foo)
+	addrCh <- l.Addr().String()
+	server.Accept(l)
+}
+
+//封装一个方法 foo，便于在 Call 或 Broadcast 之后统一打印成功或失败的日志。
+func foo(xc *xclient.XClient, ctx context.Context, typ, serviceMethod string, args *Args) {
+	var reply int
+	var err error
+	switch typ {
+	case "call":
+		err = xc.Call(ctx, serviceMethod, args, &reply)
+	case "broadcast":
+		err = xc.Broadcast(ctx, serviceMethod, args, &reply)
+	}
+	if err != nil {
+		log.Printf("%s %s error: %v", typ, serviceMethod, err)
+	} else {
+		log.Printf("%s %s success: %d + %d = %d", typ, serviceMethod, args.Num1, args.Num2, reply)
+	}
+}
+
+//call 调用单个服务实例，broadcast 调用所有服务实例
+func call(addr1, addr2 string) {
+	d := xclient.NewMultiServerDiscovery([]string{"tcp@" + addr1, "tcp@" + addr2})
+	xc := xclient.NewXClient(d, xclient.RandomSelect, nil)
+	defer func() { _ = xc.Close() }()
+	// send request & receive response
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			foo(xc, context.Background(), "call", "Foo.Sum", &Args{Num1: i, Num2: i * i})
+		}(i)
+	}
+	wg.Wait()
+}
+
+func broadcast(addr1, addr2 string) {
+	d := xclient.NewMultiServerDiscovery([]string{"tcp@" + addr1, "tcp@" + addr2})
+	xc := xclient.NewXClient(d, xclient.RandomSelect, nil)
+	defer func() { _ = xc.Close() }()
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			foo(xc, context.Background(), "broadcast", "Foo.Sum", &Args{Num1: i, Num2: i * i})
+			// expect 2 - 5 timeout
+			ctx, _ := context.WithTimeout(context.Background(), time.Second*2)
+			foo(xc, ctx, "broadcast", "Foo.Sleep", &Args{Num1: i, Num2: i * i})
+		}(i)
+	}
+	wg.Wait()
+}
+
+func main() {
+	log.SetFlags(0)
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+	// start two servers
+	go startServer(ch1)
+	go startServer(ch2)
+
+	addr1 := <-ch1
+	addr2 := <-ch2
+
+	time.Sleep(time.Second)
+	call(addr1, addr2)
+	broadcast(addr1, addr2)
 }
